74cccad0ffa83dabfd16d566adfb6f80f91cba4c
Moreno Abad <mmoren10@t-systems.es>
2023-10-09 16:01:57 +0200

    Create translations object

---
+++ src/transversal/language/translations.ts
@@ -0,0 +1,49 @@
+import { Resources } from "./Itranslations";
+
+const resources: Resources = {
+  es: {
+    translation: {
+      _01_LoginFeedbackError: "Login incorrecto",
+      _01_LoginFeedbackOk: "Login correcto",
+      _01_LoginPasswordLabel: "Password",
+      _01_LoginPasswordText: "Password",
+      _01_LoginSubmitButtonLabel: "Enviar",
+      _01_LoginSubmitButtonText: "Enviar",
+      _01_LoginTitle: "Iniciar sesión",
+      _01_LoginUserLabel: "Usuario",
+      _01_LoginUserText: "Usuario",
+      _02_ImportFeedbackError: "Importación incorrecta",
+      _02_ImportFeedbackOk: "Importación correcta",
+      _02_ImportSelectButtonLabel: "Seleccionar archivo",
+      _02_ImportSelectButtonText: "Seleccionar archivo",
+      _02_ImportSubmitButtonLabel: "Enviar archivo",
+      _02_ImportSubmitButtonText: "Enviar archivo",
+      _02_ImportTitle: "Importar archivo",
+      _03_LogoutButtonLabel: "Cerrar sesión",
+      _03_LogoutButtonText: "Cerrar sesión",
+    },
+  },
+  en: {
+    translation: {
+      _01_LoginFeedbackError: "Login wrong",
+      _01_LoginFeedbackOk: "Login correct",
+      _01_LoginPasswordLabel: "Password",
+      _01_LoginPasswordText: "Password",
+      _01_LoginSubmitButtonLabel: "Send",
+      _01_LoginSubmitButtonText: "Send",
+      _01_LoginTitle: "Log in",
+      _01_LoginUserLabel: "User",
+      _01_LoginUserText: "User",
+      _02_ImportFeedbackError: "Wrong import",
+      _02_ImportFeedbackOk: "Correct import",
+      _02_ImportSelectButtonLabel: "Select file",
+      _02_ImportSelectButtonText: "Select file",
+      _02_ImportSubmitButtonLabel: "Send file",
+      _02_ImportSubmitButtonText: "Send file",
+      _02_ImportTitle: "Import file",
+      _03_LogoutButtonLabel: "Log out",
+      _03_LogoutButtonText: "Log out",
+    },
+  },
+};
+export default resources;
